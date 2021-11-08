25f10fa0a6b642b44d258a8b138618fce2c29fd5
yogesh chavan <76579825+YOGESH606@users.noreply.github.com>
2021-11-08 11:48:57 +0530

    API Update

--- src/api/Api.js
+++ src/api/Api.js
@@ -7,7 +7,7 @@ export const FetchNews = (query) => {
     return (
         axios.get(url, {
             headers: {
-                'x-rapidapi-key': 'e441dc3763mshb311bff847e17edp10494cjsnd68579dd6ad6',
+                'x-rapidapi-key': '22675435b6msh88d453eb7c57b68p185113jsn984444b28210',
                 'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
             }
         }))
